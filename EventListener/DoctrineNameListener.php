729a4587ffaaca382d780a54a38bbbd452f27093
Filippo Lazzarini <Filoz@users.noreply.github.com>
2014-02-24 16:40:41 +0100

    custom user and group entity class

--- EventListener/DoctrineNameListener.php
+++ EventListener/DoctrineNameListener.php
@@ -9,11 +9,15 @@ class DoctrineNameListener
 {
     
     protected $prefix;
+    protected $userClass;
+    protected $groupClass;
 
     public function __construct($container)
     {
         $config         = $container->getParameter('seyon_phpbb3_user');
         $this->prefix   = $config['table_prefix'];
+        $this->userClass = $config['user_class'];
+        $this->groupClass = $config['group_class'];
     }
     
     public function loadClassMetadata(\Doctrine\ORM\Event\LoadClassMetadataEventArgs $eventArgs)
@@ -26,6 +30,10 @@ class DoctrineNameListener
             if ($mapping['type'] == \Doctrine\ORM\Mapping\ClassMetadataInfo::MANY_TO_MANY) {
                 $mappedTableName = $classMetadata->associationMappings[$fieldName]['joinTable']['name'];
                 $classMetadata->associationMappings[$fieldName]['joinTable']['name'] = $this->prefix . $mappedTableName;
+            }else if ($mapping['targetEntity'] == '\Seyon\PHPBB3\UserBundle\Entity\User') {
+            	$classMetadata->associationMappings[$fieldName]['targetEntity'] = $this->userClass;
+            } else if ($mapping['targetEntity'] == '\Seyon\PHPBB3\UserBundle\Entity\Group') {
+            	$classMetadata->associationMappings[$fieldName]['targetEntity'] = $this->groupClass;
             }
         }
     }
